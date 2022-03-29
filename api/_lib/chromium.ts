b44eccc60a0caa78bc64af1b7c0ed6dea501dad4
shabaraba <work.shabaraba@gmail.com>
2022-03-30 00:32:28 +0900

    - 日本語フォント読み込みのため遅延時間を伸ばしてみるjj:wq

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -17,10 +17,13 @@ async function getPage(isDev: boolean) {
 export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
     const page = await getPage(isDev);
     await page.setViewport({ width: 2048, height: 1170 });
+    await page.setExtraHTTPHeaders({
+        'Accept-Language': 'ja-JP'
+    });
     await page.setContent(html);
 
     const sleep = (waitTime: number) => new Promise( resolve => setTimeout(resolve, waitTime) );
-    await sleep(500); // フォント反映に時間がかかる場合がある？ので少し待つ
+    await sleep(1500); // フォント反映に時間がかかる場合がある？ので少し待つ
     const file = await page.screenshot({ type });
     return file;
 }
