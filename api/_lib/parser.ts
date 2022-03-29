20f3b4243e2af77f60919289f779b428fadeac72
shabaraba <work.shabaraba@gmail.com>
2022-03-30 08:46:05 +0900

    - ロゴ画像は一旦出さないように

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage } from 'http';
 import { parse } from 'url';
-import { ParsedRequest, Theme } from './types';
+// import { ParsedRequest, Theme } from './types';
+import { ParsedRequest } from './types';
 
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
@@ -38,7 +39,7 @@ export function parseRequest(req: IncomingMessage) {
         bg: typeof(bg) === 'string' ? bg : '',
         siteTitle: typeof(siteTitle) === 'string' ? siteTitle : ''
     };
-    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
+    // parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
     return parsedRequest;
 }
 
@@ -52,16 +53,16 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
     }
 }
 
-function getDefaultImages(images: string[], theme: Theme): string[] {
-    const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+// function getDefaultImages(images: string[], theme: Theme): string[] {
+//     const defaultImage = theme === 'light'
+//         ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
+//         : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
 
-    if (!images || !images[0]) {
-        return [defaultImage];
-    }
-    if (!images[0].startsWith('https://assets.vercel.com/') && !images[0].startsWith('https://assets.zeit.co/')) {
-        images[0] = defaultImage;
-    }
-    return images;
-}
+//     if (!images || !images[0]) {
+//         return [defaultImage];
+//     }
+//     if (!images[0].startsWith('https://assets.vercel.com/') && !images[0].startsWith('https://assets.zeit.co/')) {
+//         images[0] = defaultImage;
+//     }
+//     return images;
+// }
